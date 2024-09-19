6aff8e39fa508c93cc91aea4a13d724eb3299417
K2k-WebDeveloper <itsupport1@k2kitsupport.com>
2024-09-19 12:34:15 +0530

    uploaded audio messaging

--- story-app-backend/models/Story.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const storySchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  fileUrl: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now, expires: '24h' }, // Auto delete after 24h
-});
-
-module.exports = mongoose.model('Story', storySchema);
